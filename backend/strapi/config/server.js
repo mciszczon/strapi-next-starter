96974c516ad17615c46b4992b7c79a225322817d
Mateusz Ciszczoń <contact@mciszczon.pl>
2020-12-04 23:34:44 +0100

    Added strapi (#27)

---
+++ backend/strapi/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '43da900b0960c6d7349b688d8139370d'),
+    },
+  },
+});
